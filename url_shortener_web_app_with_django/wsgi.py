b3df3197f3701bd5c5da51bc1a09fe5e9636fc36
İbrahim YILDIZ <yildizib@gmail.com>
2023-02-19 21:42:11 +0300

    ...

---
+++ url_shortener_web_app_with_django/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for url_shortener_web_app_with_django project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "url_shortener_web_app_with_django.settings"
+)
+
+application = get_wsgi_application()
